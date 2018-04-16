28f84deb8188cea3e266a5c91064c58f3fb8bfbd
TracynOtterness <tracynot20@students.nsummit.org>
2018-04-16 13:25:25 -0600

    Country script as of solving country sorting

--- Assets/scripts/Country.cs
+++ Assets/scripts/Country.cs
@@ -1,12 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Country : MonoBehaviour {
+    public Hexagon[] hexagons;
+    public Instantiator instantiator;
+    public Hexagon[] HexesInCountry;
 
-    private Instantiator instantiator;
-
-    void Awake(){
+    private void Awake(){
+        instantiator = GameObject.FindObjectOfType<Instantiator>();
         this.name = "Country " + instantiator.countryCount;
+    }
+    public void CreateArray(){
+        hexagons = this.GetComponentsInChildren<Hexagon>();
+    }
+    public void DeleteIfEmpty(){
+        if (hexagons.Length == 0) {
+            instantiator.countrySubtraction++;
+            Destroy(this.gameObject);
+        }
     }
 }
