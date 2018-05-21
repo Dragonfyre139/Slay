95417b41483f1c34833b30f46e036080eed51149
TracynOtterness <tracynot20@students.nsummit.org>
2018-05-21 13:24:10 -0600

    HexMovement as of 5/21/18

--- Assets/scripts/HexMovement.cs
+++ Assets/scripts/HexMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,24 +8,50 @@ public class HexMovement : MonoBehaviour {
 	private int nationNum;
 	public GameObject personObject;
 	private bool valid = true;
-	private int guardedBy;
 	private Person person;
+    bool placedIsValid;
+    private Hexagon hexagon;
+
+    void Start(){
+        hexagon = this.gameObject.GetComponent<Hexagon>();
+        mouse = GameObject.Find("Mouse").GetComponent<Mouse>();
+    }
 		void OnTriggerStay2D(Collider2D col) {
-		mouse = col.GetComponent<Mouse> ();
-		if (col.gameObject.tag.Equals ("mouse") && mouse.hasPerson == true && Input.GetMouseButtonUp (0) == true) {
-			personObject = mouse.personObject;
-			person = personObject.GetComponent<Person> ();
-			if (person.hover) {
-				if (valid) {
-					mouse.hasPerson = false; 
-					guardedBy = person.tier;
-					if (this.nationNum != person.nationNum) {
-						this.nationNum = person.nationNum;
-					}
-				}
-				person.Place ();
-			} 
+        if (col.gameObject.tag.Equals("mouse") && Input.GetMouseButtonUp(0) == true && mouse.hasPerson == true)
+        {
+            personObject = mouse.personObject;
+            person = personObject.GetComponent<Person>();
+            if (person.hover)
+            {
+                placedIsValid = PlacedIsValid();
+                if (placedIsValid)
+                {
+                    mouse.hasPerson = false;
+                    hexagon.guardedBy = person.tier;
+                    person.Place();
+                }
+            }
 		}
-	}
+    }
+    bool PlacedIsValid (){ //the criteria for placing the unit down to be something the player can do
+        Hexagon thisHex = this.gameObject.GetComponent<Hexagon>();
+        // criteria for it to be a valid place to move the unit goes here
+        print("this hex is guarded by: " + thisHex.guardedBy);
+        if (thisHex.isLand == true ) { //requirements regardless of country/color of hexagon
+            if (this.gameObject.transform.parent != person.transform.parent) //requirements if it is taking a hexagon of another country
+            { 
+                if (thisHex.guardedBy < person.tier || thisHex.guardedBy == 0){
+                    return true;  
+                }
+                else
+                {
+                    print("the unit you tried to occupy is too heavily guarded for that unit to take!");
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 }
 
